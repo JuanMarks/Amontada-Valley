51124565754ad7249c37273c219fa4b3ff063098
JuanMarks <82836871+JuanMarks@users.noreply.github.com>
2022-06-09 18:34:12 -0300

    debugando o codigo

--- MinhaAgenda/minhaagenda.py
+++ MinhaAgenda/minhaagenda.py
@@ -1,10 +1,11 @@
+import re
+
 class MinhaAgenda:
     def __init__(self, nome, endereco, email, numero_contato):
         self._nome = nome
         self.endereco = endereco
         self.email = email
         self.numero_contato = numero_contato
-        self.verifica_cpf()
         self.verifica_email()
     
     @property
@@ -13,15 +14,6 @@ class MinhaAgenda:
     @nome.setter
     def nome(self,novo_nome):
         self._nome = novo_nome
-    
-    def verifica_cpf(self):
-        padrao = re.compile("[0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2}")
-        busca = padrao.search(self._cpf)
-        if busca:
-            cpf = busca.group()
-            print(f"CPF nos conformes {cpf}")
-        else:
-            print("CPF esta errado")
     
     def verifica_email(self):
         padrao = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.com+$)")
--- MinhaAgenda/pessoafisica.py
+++ MinhaAgenda/pessoafisica.py
@@ -6,6 +6,7 @@ class PessoaFisica(MinhaAgenda):
         super().__init__(nome, endereco, email, numero_contato)
         self.data_de_aniversario = data_de_aniversario
         self._cpf = cpf
+        self.verifica_cpf()
 
     @property
     def cpf(self):
@@ -14,7 +15,16 @@ class PessoaFisica(MinhaAgenda):
     def cpf(self, novo_cpf):
         self._cpf = novo_cpf
     
+    def verifica_cpf(self):
+        padrao = re.compile("[0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2}")
+        busca = padrao.search(self._cpf)
+        if busca:
+            cpf = busca.group()
+            print(f"CPF nos conformes {cpf}")
+        else:
+            print("CPF esta errado")
+    
     def __str__(self):
-        return f" \n Data de Aniversario: {self.data_de_aniversario} \n CPF: {self.cpf}"
+        return f" \n Data de Aniversario: {self.data_de_aniversario} \n CPF: {self._cpf}"
 
 pessoa = PessoaFisica("nome","endereco","juanpaulo77@gmail.com", "numero_contato", "data de aniversario", "061.351.563-30")
