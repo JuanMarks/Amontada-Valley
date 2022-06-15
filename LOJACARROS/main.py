306cebc0fef8b418a7d1f815dd6403f5e00ca265
JuanMarks <82836871+JuanMarks@users.noreply.github.com>
2022-06-15 18:32:00 -0300

    atualizando main.py da loja de carros

--- LOJACARROS/main.py
+++ LOJACARROS/main.py
@@ -6,22 +6,19 @@ suv = SUV("SUV", 15000)
 conv = Conversivel("Conversivel", 18000)
 sport = Esportivos("Ferrari", 20000)
 
-lista = [sedan, suv, conv, sport, suv]
+lista = [sedan, suv, conv, sport, suv, sedan]
 
 print("BEM VINDO A LOJA AUTOMATICA DE CARROS \n")
 nome = input("Agora digite seu nome:  ")
 comprador = Comprador(nome, 50000)
 
 print(f"Ola {comprador.nome} aqui esta a lista dos carros disponiveis para a venda")
-#print(f"(1) {sedan} \n (2) {suv} \n (3) {conv} \n (4) {sport}")
+
 i = 0
 for carro in lista:
     i += 1
     print(f"({i}) {carro} \n")
 
 resposta = int(input("Qual o carro voce vai comprar? \n Digite aqui sua resposta:  "))
-
-for n in range(0 , len(lista)):
-    if resposta == n + 1:
-        comprador.comprar_carro(lista[n], lista)
+comprador.comprar_carro(lista[resposta-1], lista)
         
